83444a1a1f46962f88d4edcc9f3281b4b60f4854
Gliting29 <psai4580@gmail.com>
2021-08-09 09:45:56 +0530

    added carousel and grd

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -2,7 +2,6 @@ import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:tride_mobility/constants/colours.dart';
-import 'package:tride_mobility/utils/CarouselContainer.dart';
 import 'package:tride_mobility/utils/app_bar.dart';
 
 class HomeScreen extends StatefulWidget {
@@ -11,127 +10,173 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
-
   List<dynamic> photoURl = [
-  {
-    "photo":"images/tride_icon.jpeg"
-  },
-    {
-      "photo":"images/tride_icon.jpeg"
-    },
-    {
-      "photo":"images/tride_icon.jpeg"
-    },
-    {
-      "photo":"images/tride_icon.jpeg"
-    },
+    {"photo": "images/tride_icon.jpeg"},
+    {"photo": "images/tride_icon.jpeg"},
+    {"photo": "images/tride_icon.jpeg"},
+    {"photo": "images/tride_icon.jpeg"},
   ];
 
   List<dynamic> itemList = [
-    {
-      "photo":"images/tride_icon.jpeg",
-      "prodname":"ABC",
-      "price": "100"
-    },
-    {
-      "photo":"images/tride_icon.jpeg",
-      "prodname":"DEF",
-      "price": "200"
-    },
-    {
-      "photo":"images/tride_icon.jpeg",
-      "prodname":"GHI",
-      "price": "300"
-    },
-    {
-      "photo":"images/tride_icon.jpeg",
-      "prodname":"JKL",
-      "price": "400"
-    },
+    {"photo": "images/tride_icon.jpeg", "prodname": "ABC", "price": "100"},
+    {"photo": "images/tride_icon.jpeg", "prodname": "DEF", "price": "200"},
+    {"photo": "images/tride_icon.jpeg", "prodname": "GHI", "price": "300"},
+    {"photo": "images/tride_icon.jpeg", "prodname": "JKL", "price": "400"},
+    {"photo": "images/tride_icon.jpeg", "prodname": "ABC", "price": "100"},
+    {"photo": "images/tride_icon.jpeg", "prodname": "DEF", "price": "200"},
+    {"photo": "images/tride_icon.jpeg", "prodname": "GHI", "price": "300"},
+    {"photo": "images/tride_icon.jpeg", "prodname": "nav", "price": "400"},
   ];
+  int _current = 0;
 
   @override
   Widget build(BuildContext context) {
-    double screenHeight = MediaQuery.of(context).size.height;
-    double screenWidth = MediaQuery.of(context).size.width;
     return Scaffold(
       appBar: PreferredSize(
         preferredSize: Size.fromHeight(70),
-        child: AppBarWidget(
-          title1: "Tride",
-          title2: "Mobility"
-        ),
-
+        child: AppBarWidget(title1: "Tride", title2: "Mobility"),
       ),
       backgroundColor: screenBackground2,
       body: Container(
-        height: screenHeight,
-        width: screenWidth,
         child: ListView(
           physics: BouncingScrollPhysics(),
+          shrinkWrap: true,
           children: [
-            Container(
-              //color: screenBackground,
-              child: CarouselSlider.builder(
-                options: CarouselOptions(
-                  height: 200,
-                  enableInfiniteScroll: true,
-                  autoPlay: true,
-                  autoPlayInterval: Duration(seconds: 3),
-                  autoPlayAnimationDuration: Duration(milliseconds: 800),
-                  autoPlayCurve: Curves.fastOutSlowIn,
-                  viewportFraction: 0.6,
-                ),
-                itemCount: photoURl.length,
-                itemBuilder: (BuildContext context , int index, int pageIndex) => Container(
-                    decoration: BoxDecoration(
-                       color: screenBackground,
-                      borderRadius: BorderRadius.circular(20),
-                    ),
-                    margin: EdgeInsets.all(10),
-                    alignment: Alignment.center,
-                    height: 150,
-                    width: 150,
-                    child: Image.asset(photoURl[index]['photo'],alignment: Alignment.center,fit: BoxFit.fill,)),
+           Column(
+             children: [
+               Container(
+                 //color: screenBackground,
+                 child: CarouselSlider.builder(
+                   options: CarouselOptions(
+                       height: 200,
+                       enableInfiniteScroll: true,
+                       autoPlay: true,
+                       autoPlayInterval: Duration(seconds: 3),
+                       autoPlayAnimationDuration: Duration(milliseconds: 800),
+                       autoPlayCurve: Curves.fastOutSlowIn,
+                       viewportFraction: 0.6,
+                       onPageChanged: (index,reason) {
+                         setState(() {
+                           _current=index;
+                         });
+                       }
+                   ),
+                   itemCount: photoURl.length,
+                   itemBuilder: (BuildContext context, int index, int pageIndex) =>
+                       Container(
+                           margin: EdgeInsets.symmetric(horizontal: 20,vertical: 20),
+                           child: ClipRRect(
+                             borderRadius: BorderRadius.all(
+                               Radius.circular(5.0),
+                             ),
+                             child: Image(
+                               height: 100,
+                               width: 300,
+                               fit: BoxFit.fill,
+                               image: AssetImage(
+                                 photoURl[index]['photo'],
+                               ),
+                             ),
+                           )),
+                 ),
+               ),
+               Row(
+                 mainAxisAlignment: MainAxisAlignment.center,
+                 children: photoURl.map((url) {
+                   int index = photoURl.indexOf(url);
+                   return Container(
+                     width: 8,
+                     height: 8,
+                     margin: EdgeInsets.symmetric(
+                       vertical: 10,
+                       horizontal: 3,
+                     ),
+                     decoration: BoxDecoration(
+                       shape: BoxShape.circle,
+                       color: _current == index
+                           ? Color.fromRGBO(0, 0, 0, 0.9)
+                           : Color.fromRGBO(0, 0, 0, 0.4),
+                     ),
+                   );
+                 }).toList(),
+               ),
+               ConstrainedBox(
+                 constraints: BoxConstraints(
+                   maxHeight: double.infinity,
+                 ),
+                 child: Container(
+                   margin: EdgeInsets.symmetric(vertical: 20,horizontal: 20),
+                   child: GridView.count(
+                     crossAxisCount: 2,
+                     crossAxisSpacing: 10,
+                     mainAxisSpacing: 10,
+                     shrinkWrap: true,
+                     scrollDirection: Axis.vertical,
+                     physics: NeverScrollableScrollPhysics(),
+                     children: itemList.map((item) => Card(
+                       color: Colors.transparent,
+                       elevation: 0,
+                       child: Container(
+                         decoration: BoxDecoration(
+                             borderRadius: BorderRadius.circular(20),
+                             image: DecorationImage(
+                                 image: AssetImage(item['photo']),
+                                 fit: BoxFit.cover
+                             )
+                         ),
+                         child: Stack(
+                           children: [
+                             Positioned(
+                                 left: 0.0,
+                                 right: 0.0,
+                                 bottom: 0.0,
+                                 child:  Container(
+                                  // alignment: Alignment.bottomLeft,
+                                   decoration: BoxDecoration(
+                                     borderRadius: BorderRadius.only(bottomRight: Radius.circular(10),bottomLeft: Radius.circular(10)),
+                                     gradient: LinearGradient(
+                                         colors: [
+                                           primaryBackground,
+                                           secondaryBackground
+                                         ],
+                                         begin: Alignment.topLeft,
+                                         end: Alignment.bottomRight
+                                     ),
+                                   ),
+                                   child: Container(
+                                     padding: EdgeInsets.only(left: 10),
+                                     child: Text(
+                                       item['prodname'],
+                                       style: TextStyle(fontSize: 20,fontWeight: FontWeight.bold,color: screenBackground),
+                                     ),
+                                   ),
+                                 ),),
+                             Positioned(
+                               right: 0.0,
+                               bottom: 0.0,
+                               child:  Container(
+                                 child: Padding(
+                                   padding: EdgeInsets.only(right: 10),
+                                   child: Text(
+                                     "Rs." + item['price'],
+                                     style: TextStyle(fontSize: 20,color: screenBackground),
+                                   ),
+                                 ),
+                               ),),
+                           ],
+                         ),
+                       ),
+                     )).toList(),
+                   ),
 
-              ),
-            ),
-            GridView.builder(
-              shrinkWrap: true,
-              gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 2),
-              itemBuilder: (_, index) => Container(
-                margin: EdgeInsets.only(left: 10,right: 10,bottom: 10,top:10),
-                child: Column(
-                  children: [
-                   Text(itemList[index]['prodname'] ?? "svfinj",
-                   style: TextStyle(
-                     fontWeight: FontWeight.bold,
-                     fontSize: 25,
-                     color: primaryBackground
-                   ),),
-                    Container(
-                      height: 80,
-                      width: 80,
-                        margin: EdgeInsets.all(10),
-                        alignment: Alignment.center,
-                        child: Image.asset(itemList[index]['photo'] ?? "fsdoibn",alignment: Alignment.center,fit: BoxFit.fill,)
-                    ),
-                    Text("Rs. " + itemList[index]['price'] ?? "sfdonl",
-                      style: TextStyle(
-                          fontWeight: FontWeight.bold,
-                          fontSize: 25,
-                          color: secondaryBackground
-                      ),),
-                  ],
-                ),
-                color: screenBackground,
-              ),
-              itemCount: 4,
-            )
+                 ),
+               ),
+             ],
+           ),
+
           ],
         ),
       ),
-
     );
   }
 }
